53a1b1731f185be8ab292d13b59b91c35df1a9b6
Caitlin Ramsey <caitlinramsey@Caitlins-MacBook-Pro.local>
2023-04-18 15:04:58 -0400

    worked on JS and getting vars set up and defined

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,7 @@ var myApiKey = '7869685bf7ac61c2e9b6bea2b3c4883f';
 var requestURL = 'https://api.openweathermap.org/data/2.5/weather?q=';
 var oneCall = 'https://api.openweathermap.org/data/2.5/onecall?lat='
 var searchHistory = $('#search-history');
+var cityIconUrl = 'http://openweathermap.org/img/wn/';
 var loadResultsHistoryArray = loadResultsHistory();
 var cityInput = $('#city');
 var fiveDayForecast = $('#five-day-forecast');
@@ -75,6 +76,73 @@ function getForecast(city) {
                                         .attr({
                                             id: 'current-weather'
                                         })
+                                    var cityIcon = weatherData.current.weather[0].icon;
+                                    var currentCityIcon = cityIconUrl + cityIcon + '.png';
+
+                                    var forecastHeading = $('<h2>')
+                                        text.(city + ' (' + currentDay + ')');
+                                    var iconImage = $('<img>')
+                                        .attr({
+                                            id: forecast-icon',
+                                            src: currentCityIcon,
+                                            alt: 'Forecast Weather Icon'
+                                        })
+                                    var forecastList = $('<ul>')
+
+                                    var forecastDetails = ['Temperature: ' + weatherData.current.temp + ' °F', 'Wind: ' + weatherData.current.wind_speed + ' MPH', 'Humidity: ' + weatherData.current.humidity + '%', 'UV Index: ' + weatherData.current.uvi]
+
+                                    for (var i = 0; i < forecastDetails.length; i++) {
+                                        if (forecastDetails[i] === 'UV Index: ' + weatherData.current.uvi) {
+                                            var forecastListItem = $('<li>')
+                                                .text('UV Index: ')
+
+                                            forecastList.append(forecastListItem);
+
+                                            var uviSpan = $('<span>')
+                                                .text(weatherData.current.uvi);
+
+                                            if (uviSpan.text() <= 2) {
+                                                uviSpan.addClass('favorable');
+                                            } else if (uviSpan.text() > 2 && uviSpan.text() <= 7) {
+                                                uviSpan.addClass('moderate');
+                                            } else {
+                                                uviSpan.addClass('severe');
+                                            }
+
+                                            forecastListItem.append(uviSpan);
+
+                                        } else {
+                                            var forecastListItem = $('<li>')
+                                                .text(forecastDetails[i])
+                                            
+                                            forecastList.append(forecastListItem);
+                                        }
+                                    }
+
+                                    $('#five-day-forecast').before(currentForecast);
+                                    currentForecast.append(forecastHeading);
+                                    forecastHeading.append(iconImage);
+                                    currentForecast.append(forecastListItem);
+
+                                    var fiveDayForecastHeading = $('<h2>')
+                                        .text('5-Day Forecast:')
+                                        .attr({
+                                            id: 'five-day-forecast-header'
+                                        })
+                                    
+                                    $('#current-weather').after(fiveDayForecastHeading)
+
+                                    var fiveDayForecastArray = [];
+
+                                    for (var i = 0; i < 5; i++) {
+                                        let forecastDate = moment().add(i + 1, 'days').format('M/DD/YYYY');
+
+                                        fiveDayForecastArray.push(forecastDate);
+                                    }
+
+                                    for (var i = 0; i < fiveDayForecastArray.length; i++) {
+                                        var 
+                                    }
                                 })
                             }
                         })
