2ec9e567eebf765b913ea128cb1fea6fcbb3b5b0
Caitlin Ramsey <caitlinramsey@Caitlins-MacBook-Pro.local>
2023-04-18 13:40:25 -0400

    added weather api

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,12 @@
 var input = document.createElement('input');
 input.type - 'text';
 CSSContainerRule.appendChild(input);
+
+function getApi() {
+    var requestURL = "api.openweathermap.org/data/2.5/forecast?q=Raleigh,us&appid=7869685bf7ac61c2e9b6bea2b3c4883f";
+}
+
+fetch(requestURL)
+    .then(function (response) {
+        return.response.json();
+    })
